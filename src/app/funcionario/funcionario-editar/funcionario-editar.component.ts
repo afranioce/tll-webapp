352561ad9451f5ff88b260b3c39a460834b69e29
Afranio Martins <afranio.martins@fariasbrito.com.br>
2018-07-20 13:19:50 -0300

    feat(funcionario): Cria form para editar

--- src/app/funcionario/funcionario-editar/funcionario-editar.component.ts
+++ src/app/funcionario/funcionario-editar/funcionario-editar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Funcionario } from '../../core/models/funcionario.model';
+import { FuncionarioService } from '../../core/service/funcionario.service';
+import { Departamento } from '../../core/models/departamento.model';
 
 @Component({
   selector: 'app-funcionario-editar',
@@ -6,10 +11,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./funcionario-editar.component.scss']
 })
 export class FuncionarioEditarComponent implements OnInit {
+  funcionarioForm: FormGroup;
+  funcionario: Funcionario = new Funcionario();
+  departamentos: Departamento[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder,
+    private funcionarioService: FuncionarioService,
+    private route: ActivatedRoute
+  ) {
+    this.criarFormulario();
   }
 
+  async ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.funcionario = await this.funcionarioService.get(id);
+
+    this.funcionarioForm.setValue({
+      nome: this.funcionario.nome,
+      departamento: this.funcionario.departamento.id
+    });
+
+    this.departamentos = [
+      {
+        id: 1,
+        nome: 'Departamento 1'
+      },
+      {
+        id: 2,
+        nome: 'Departamento 2'
+      }
+    ];
+  }
+
+  criarFormulario() {
+    this.funcionarioForm = this.fb.group({
+      nome: [this.funcionario.nome, [Validators.required, Validators.maxLength(200)]],
+      departamento: [this.funcionario.departamento.id, Validators.required]
+    });
+  }
+
+  enviar() {
+    if (this.funcionarioForm.invalid) {
+      return;
+    }
+
+    const funcionario = this.fromForm2Entity(this.funcionarioForm.value);
+    this.funcionarioService.update(funcionario.id, funcionario);
+  }
+
+  fromForm2Entity(formValue: any): Funcionario {
+    this.funcionario.nome = formValue.nome;
+    this.funcionario.departamento = formValue.departamento;
+    return this.funcionario;
+  }
 }
