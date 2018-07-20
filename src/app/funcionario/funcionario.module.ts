1dac332e69557857e845272b8dfb6566d702ae76
Afranio Martins <afranio.martins@fariasbrito.com.br>
2018-07-20 13:18:15 -0300

    fix(funcionario): Importa modulo do material para botão

--- src/app/funcionario/funcionario.module.ts
+++ src/app/funcionario/funcionario.module.ts
@@ -7,8 +7,16 @@ import { FuncionarioCriarComponent } from './funcionario-criar/funcionario-criar
 import { FuncionarioEditarComponent } from './funcionario-editar/funcionario-editar.component';
 import { FuncionarioDetalheComponent } from './funcionario-detalhe/funcionario-detalhe.component';
 import { FuncionarioComponent } from './funcionario.component';
-import { CovalentDataTableModule } from '../../../node_modules/@covalent/core/data-table';
-import { MatCardModule, MatIconModule, MatSelectModule, MatInputModule, MatFormFieldModule, MatOptionModule } from '@angular/material';
+import { CovalentDataTableModule } from '@covalent/core/data-table';
+import {
+  MatCardModule,
+  MatIconModule,
+  MatSelectModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatOptionModule,
+  MatButtonModule
+} from '@angular/material';
 import { FuncionarioService } from '../core/service/funcionario.service';
 
 @NgModule({
@@ -19,6 +27,7 @@ import { FuncionarioService } from '../core/service/funcionario.service';
 
     CovalentDataTableModule,
 
+    MatButtonModule,
     MatCardModule,
     MatIconModule,
     MatFormFieldModule,
